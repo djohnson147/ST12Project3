87c2c73db7ba425ba854c0136f06b857d3b19ec1
Selanokay <grrvvt@umsystem.edu>
2023-10-25 15:05:59 -0500

    Created functions for program clarity, created get_Symbol function. Commented out section is for testing, will return to create error exception later

--- stockData.py
+++ stockData.py
@@ -1,6 +1,26 @@
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GOOGL&interval=5min&apikey=QNBSQ569PDAHAV41'
-r = requests.get(url)
-data = r.json()
-print(data)
+key = "QNBSQ569PDAHAV41"
+
+def get_Symbol():
+    global userSymbol 
+    userSymbol = input("Enter the stock symbol you are looking for: ")
+    #if (userSymbol == "GOOGL"):
+    #    print("Works")
+    #else:
+    #    print("Doesn't Work")
+
+
+
+def main():
+    get_Symbol()
+    getData()
+
+def getData():
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + userSymbol + '&interval=5min&apikey=' + key
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+
+
+main()
